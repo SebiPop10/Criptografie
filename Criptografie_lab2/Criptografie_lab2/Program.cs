5a2276f7b572a3d7590f96889fa3146acfe028fe
SebiPop10 <92030844+SebiPop10@users.noreply.github.com>
2024-12-10 15:00:32 +0200

    Add files via upload

---
+++ Criptografie_lab2/Criptografie_lab2/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Cifrul Vigenere. Cifrul substitutie polialfabetica
+
+//Console.WriteLine("Introduceti textul: ");
+//Console.WriteLine("Introduceti cheia: ");
+//Console.WriteLine("Textul criptat este: ");
+//Console.WriteLine("Textul decriptat este: ");
+// Exemplu de utilizare
+using Criptografie_lab2;
+
+VigenereCypher cipher = new VigenereCypher("oculorhinolaringology");
+
+string textDeCriptat = "attacking tonight";
+string criptat = cipher.Encrypt(textDeCriptat);
+Console.WriteLine("Text criptat: " + criptat);
+
+string decriptat = cipher.Decrypt(criptat);
+Console.WriteLine("Text decriptat: " + decriptat);
---
+++ Criptografie_lab2/Criptografie_lab2/VigenereCypher.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Criptografie_lab2
+{
+    public class VigenereCypher
+    {
+        private string key;
+
+        public VigenereCypher(string key)
+        {
+            this.key = key.ToUpper(); 
+        }
+
+        
+        public string Encrypt(string plaintext)
+        {
+            string ciphertext = "";
+            plaintext = plaintext.ToUpper(); 
+            int keyIndex = 0;
+
+            for (int i = 0; i < plaintext.Length; i++)
+            {
+                char currentChar = plaintext[i];
+
+                
+                if (char.IsLetter(currentChar))
+                {
+                    int letterPosition = currentChar - 'A';
+                    int keyPosition = key[keyIndex % key.Length] - 'A';
+
+                    
+                    char encryptedChar = (char)((letterPosition + keyPosition) % 26 + 'A');
+                    ciphertext += encryptedChar;
+
+                    keyIndex++; 
+                }
+                else
+                {
+                    
+                    ciphertext += currentChar;
+                }
+            }
+
+            return ciphertext;
+        }
+
+       
+        public string Decrypt(string ciphertext)
+        {
+            string plaintext = "";
+            ciphertext = ciphertext.ToUpper(); 
+            int keyIndex = 0;
+
+            for (int i = 0; i < ciphertext.Length; i++)
+            {
+                char currentChar = ciphertext[i];
+
+                
+                if (char.IsLetter(currentChar))
+                {
+                    int letterPosition = currentChar - 'A';
+                    int keyPosition = key[keyIndex % key.Length] - 'A';
+
+                    
+                    char decryptedChar = (char)((letterPosition - keyPosition + 26) % 26 + 'A');
+                    plaintext += decryptedChar;
+
+                    keyIndex++; 
+                }
+                else
+                {
+                    
+                    plaintext += currentChar;
+                }
+            }
+
+            return plaintext;
+        }
+    }
+}
